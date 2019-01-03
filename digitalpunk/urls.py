40882402a58aac77d71626f7dfb0d0d2fc354c7d
volkan@clouddemo01.com <volkanv@au1.ibm.com>
2019-01-03 19:46:25 +1100

    remove vue frontend, use only django for blog management

--- digitalpunk/urls.py
+++ digitalpunk/urls.py
@@ -1,4 +1,4 @@
-"""digitalpunk URL Configuration
+"""blog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -14,12 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
-from .routers import router
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-    path('', TemplateView.as_view(template_name='application.html'), name='app'),
 ]
